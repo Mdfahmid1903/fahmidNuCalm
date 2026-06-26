aaca612d4bc798601decf0ab12b2b70d142f3e73
fahmid <fahmidhrm@gmail.com>
2026-06-27 00:52:00 +0600

    hero edit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('.menu');
 const options = document.querySelector('.options');
+const icon = document.querySelector('.fa-xmark');
 
 menu.addEventListener('click', () => {
   options.classList.toggle('active');
@@ -13,24 +14,35 @@ menu.addEventListener('click', () => {
 
 function loadVideo() {
   const wrapper = document.getElementById('videoWrapper');
-  wrapper.innerHTML = `
-     <iframe
-          title="vimeo-player"
-          src="https://player.vimeo.com/video/1133224775?h=7e30dbcba3"
-          width="640"
-          height="360"
-          frameborder="0"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allow="
-            autoplay;
-            fullscreen;
-            picture-in-picture;
-            clipboard-write;
-            encrypted-media;
-            web-share;
-            accelerometer;
-          "
-            allowfullscreen;
-            
-        ></iframe>`;
+  const heroVideo = document.querySelector('.hero-video');
+
+  if (heroVideo.classList.contains('display')) {
+    heroVideo.classList.remove('display');
+     heroVideo.innerHTML = ` <iframe
+        title="vimeo-player"
+        src="https://player.vimeo.com/video/1133224775?h=7e30dbcba3"
+        width="640"
+        height="360"
+        frameborder="0"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allow="
+          autoplay;
+          fullscreen;
+          picture-in-picture;
+          clipboard-write;
+          encrypted-media;
+          web-share;
+        "
+        allowfullscreen
+      ></iframe>
+      <i class="fa-solid fa-xmark" ></i>
+      `;
+  } else {
+    heroVideo.classList.add('display');
+    heroVideo.innerHTML = '';
+  }
 }
+
+icon.addEventListener('click', () => {
+  loadVideo();
+});
